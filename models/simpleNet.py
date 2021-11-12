004f6c3f3e5ab9648102bd841eff1378f30e5149
pp <1726636370@qq.com>
2021-11-12 20:08:44 +0800

    simplenet

--- models/simpleNet.py
+++ models/simpleNet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+from torch.nn.modules.activation import ReLU
 
 from models.base_models.resnet import *
 
@@ -12,16 +13,8 @@ class SimpleNet(nn.Module):
         super().__init__()
         self.spatial_path=SpatialPath()
         self.context_path=ContextPath(pretrained_base,**kwargs)
-        self.decoder=nn.Sequential(
-            nn.Conv2d(512,256,3,1,1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(256,128,3,1,1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Dropout(0.1)
-        )
-        self.cls_conv=nn.Conv2d(128,num_class,1,1)
+        self.decoder=Decoder()
+        self.cls_conv=nn.Conv2d(256,num_class,1,1,padding=0)
 
     def forward(self,x):
         h,w=x.size()[2:]
@@ -29,9 +22,9 @@ class SimpleNet(nn.Module):
         spatial_out=self.spatial_path(x)
         context_out=self.context_path(x)
 
-        x=torch.cat(spatial_out,context_out)
+        x=torch.cat((spatial_out,context_out),dim=1)
 
-        x=self.decoder(x)
+        x=self.decoder(x)#channels 1280
 
         x=self.cls_conv(x)
 
@@ -42,28 +35,24 @@ class SimpleNet(nn.Module):
 class SpatialPath(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1=nn.Conv2d(3,64,  3,2,1)
-        self.bn1=nn.BatchNorm2d(64)
-        self.relu=nn.ReLU()
-        self.conv2=nn.Conv2d(64,128,3,2,1)
-        self.bn2=nn.BatchNorm2d(128)
-        self.conv3=nn.Conv2d(128,256,3,2,1)
-        self.bn3=nn.BatchNorm2d(256)
-        self.conv3=nn.Conv2d(128,512,3,2,1)
-        self.bn3=nn.BatchNorm2d(512)
+        self.head=nn.Sequential(
+            nn.Conv2d(3,64,3,2,1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.Conv2d(64,128,3,2,1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.Conv2d(128,256,3,2,1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.Conv2d(256,512,3,2,1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),            
+            nn.Dropout(0.1)
+        )
 
     def forward(self,x):
-        x=self.conv1(x)
-        x=self.bn1(x)
-        x=self.relu(x)
-        x=self.conv2(x)
-        x=self.bn2(x)
-        x=self.relu(x)
-        x=self.conv3(x)
-        x=self.bn3(x)
-        x=self.relu(x)
-
-        return x
+        return self.head(x)
 
 class ContextPath(nn.Module):
     def __init__(self,pretrained_base,**kwargs):
@@ -85,10 +74,61 @@ class ContextPath(nn.Module):
         x=self.maxpool(x)
         x=self.layer1(x)
         x=self.layer2(x)
-        x=self.layer3(x)
-        x=self.layer4(x)
+        c3=self.layer3(x)
+        c4=self.layer4(c3)
+        c4=F.interpolate(c4,c3.size()[2:],mode='bilinear',align_corners=True)
+
+        res=torch.cat((c3,c4),dim=1)
+
+        return res
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1=nn.Sequential(
+            nn.Conv2d(1280,512,3,1,1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True)
+        )        
+        self.upsample=nn.UpsamplingBilinear2d(scale_factor=2)
+        self.conv2=nn.Sequential(
+            nn.Conv2d(512,256,3,1,1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True)
+        )
+        self.conv3=nn.Sequential(
+            nn.Conv2d(256,256,1,1,1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True)
+        )
+        self.conv4=nn.Sequential(
+            nn.Conv2d(256,256,1,1,1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True)
+        )
+        self.conv5=nn.Sequential(
+            nn.Conv2d(256,256,1,1,1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True)
+        )
+        self.conv6=nn.Sequential(
+            nn.Conv2d(256,256,1,1,1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True)
+        )
+    def forward(self,x):
+        x=self.conv1(x)        
+        x=self.conv2(x)        
+        x=self.conv3(x)
+        x=self.upsample(x)                
+        x=self.conv4(x)   
+        x=self.upsample(x)     
+        x=self.conv5(x)   
+        x=self.upsample(x)     
+        x=self.conv6(x)        
 
         return x
+
 
 if __name__=='__main__':
     pass
