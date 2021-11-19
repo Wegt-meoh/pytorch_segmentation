1731fce97935d4c3801efecba7f73eedfe640662
pp <1726636370@qq.com>
2021-11-19 08:42:27 +0800

    config

--- Configer.py
+++ Configer.py
@@ -4,18 +4,18 @@ def get_parsed_args():
     args=argparse.ArgumentParser(description="segmentation parameter")
 
     args.add_argument("--model",type=str,default='bisenet')
-    args.add_argument('--pretrained_model',type=str,default='/home/deep1/QuePengbiao/result/simplenet_resnet34_cvc_voc/simplenet_resnet34_cvc_voc_120',
+    args.add_argument('--pretrained_model',type=str,default='/home/deep1/QuePengbiao/result/bisenet_resnet34_pascal_voc/models/480.pth',
                         help='only used in eval')
     args.add_argument("--backbone",type=str,default='resnet34')
     args.add_argument("--pretrained_base",type=bool,default=True)
     args.add_argument("--backbone_dir",type=str,default='/home/deep1/QuePengbiao/pretrain_models')
     args.add_argument("--lr",type=float,default=1e-4)
 
-    args.add_argument("--dataset",type=str,default='cvc_voc')
-    args.add_argument("--epoch",type=int,default=120)
-    args.add_argument("--batch_size",type=int,default=6)
-    args.add_argument("--base_size",type=int,default=220)
-    args.add_argument("--crop_size",type=int,default=230)
+    args.add_argument("--dataset",type=str,default='pascal_voc')
+    args.add_argument("--epoch",type=int,default=1000)
+    args.add_argument("--batch_size",type=int,default=8)
+    args.add_argument("--base_size",type=int,default=513)
+    args.add_argument("--crop_size",type=int,default=513)
     args.add_argument("--workers",type=int,default=8)
 
     args.add_argument("--device",type=str,default='cuda',choices=['cuda','cpu'])
