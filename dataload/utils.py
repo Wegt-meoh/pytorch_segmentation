79d17e541e4caeb31c0d0290a161aa9f2abc8198
pp <1726636370@qq.com>
2021-11-19 08:42:09 +0800

    utils

--- dataload/utils.py
+++ dataload/utils.py
@@ -6,7 +6,9 @@ def save_pred(pred,path,filename,dataset):
         impred=Image.fromarray(pred)        
         if dataset=='cvc_voc':
             impred.putpalette(cvc_palette())
-        
+        elif dataset=='pascal_voc':
+            impred.putpalette(_getvocpallete(256))
+
         if not os.path.exists(path):
             os.makedirs(path)
         impred.save(path+'/'+filename+'.png')
@@ -17,3 +19,20 @@ def cvc_palette():
     for i in range(256*3-6):
         palette.append(0)
     return palette
+
+def _getvocpallete(num_cls):
+    n = num_cls
+    pallete = [0] * (n * 3)
+    for j in range(0, n):
+        lab = j
+        pallete[j * 3 + 0] = 0
+        pallete[j * 3 + 1] = 0
+        pallete[j * 3 + 2] = 0
+        i = 0
+        while (lab > 0):
+            pallete[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
+            pallete[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
+            pallete[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
+            i = i + 1
+            lab >>= 3
+    return pallete
