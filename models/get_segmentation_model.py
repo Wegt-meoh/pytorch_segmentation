43a07b738f2ee9b4d3943245b8961c6c52aff364
pp <1726636370@qq.com>
2021-11-22 18:24:48 +0800

    get model

--- models/get_segmentation_model.py
+++ models/get_segmentation_model.py
@@ -1,10 +1,12 @@
 from models.simpleNet import SimpleNet
 from models.bisenet import BiSeNet
+from models.bisenetv import BiSeNetV
 
 
 models={
     'simplenet':SimpleNet,
-    'bisenet':BiSeNet
+    'bisenet':BiSeNet,
+    'bisenetv':BiSeNetV
 }
 
 def get_segmentation_model(name,**kwargs):
