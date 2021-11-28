14f100d42fae1cf1089d6e0f7b11f3cdc1e91202
pp <1726636370@qq.com>
2021-11-28 20:08:25 +0800

    get dataset

--- dataload/get_segmentatio_dataset.py
+++ dataload/get_segmentatio_dataset.py
@@ -1,10 +1,13 @@
 from dataload.pascal_voc import VOCSegmentation
 from dataload.cvc_voc import CVCSegmentation
+from dataload.coco_voc import COCOSegmentation
 
-datasets={
-    "pascal_voc":VOCSegmentation,
-    "cvc_voc":CVCSegmentation
+datasets = {
+    "pascal_voc": VOCSegmentation,
+    "cvc_voc": CVCSegmentation,
+    'coco': COCOSegmentation,
 }
 
-def get_segmentation_dataset(name,**kwargs):
+
+def get_segmentation_dataset(name, **kwargs):
     return datasets[name.lower()](**kwargs)
