159f2f97102f40600d268a69a6b4365bd1a9d494
pp <1726636370@qq.com>
2021-12-06 15:55:20 +0800

    get model

--- models/get_segmentation_model.py
+++ models/get_segmentation_model.py
@@ -2,14 +2,17 @@ from models.simpleNet import SimpleNet
 from models.bisenet import BiSeNet
 from models.bisenetv import BiSeNetV
 from models.bisenetvv import BiSeNetVV
+from models.deeplabv3plus import deeplabv3plus
 
 
-models={
-    'simplenet':SimpleNet,
-    'bisenet':BiSeNet,
-    'bisenetv':BiSeNetV,
-    'bisenetvv':BiSeNetVV
+models = {
+    'simplenet': SimpleNet,
+    'bisenet': BiSeNet,
+    'bisenetv': BiSeNetV,
+    'bisenetvv': BiSeNetVV,
+    'deeplabv3plus': deeplabv3plus
 }
 
-def get_segmentation_model(name,**kwargs):
+
+def get_segmentation_model(name, **kwargs):
     return models[name.lower()](**kwargs)
