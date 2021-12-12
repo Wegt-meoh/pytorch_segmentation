10078fe6ad96b935ca69133f5ae8d2f2290ba2a3
pp <1726636370@qq.com>
2021-12-12 13:07:39 +0800

    get dataset

--- dataload/get_segmentatio_dataset.py
+++ dataload/get_segmentatio_dataset.py
@@ -1,11 +1,15 @@
 from dataload.pascal_voc import VOCSegmentation
 from dataload.cvc_voc import CVCSegmentation
 from dataload.coco_voc import COCOSegmentation
+from dataload.pascal_aug import VOCAugSegmentation
+from dataload.cityscapes import CitySegmentation
 
 datasets = {
     "pascal_voc": VOCSegmentation,
     "cvc_voc": CVCSegmentation,
     'coco': COCOSegmentation,
+    'pascal_aug': VOCAugSegmentation,
+    'cityscapes': CitySegmentation,
 }
 
 
